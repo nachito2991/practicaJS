0a92d6b76f41a11bf77d9854886f1b128a9ae60e
nachosantoro <nachosantoro2991@gmail.com>
2022-09-03 01:19:44 -0300

    primer entregable

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
-/*
- for (let N = 1 ; N <= 10; N++) {
-    
-    alert (N);
+
+alert("Ingrese la opcion del producto que desea llevar, para salir ingrese 0")
+let seleccionarProductos = Number(prompt( "1-Kilo $1200 2-Medio kilo $750 3-Cuarto kilo $450 4-Cucurucho $300"))
+let seleccionarCantidad;
+let total = 0;
+
+
+const cantidad = (cant, precio) => {
+  return cant * precio
 }
-*/
 
 
+while (seleccionarProductos != 0) {
+  switch (seleccionarProductos) {
+    case 1:
+      seleccionarCantidad= Number(prompt("el producto seleccionado un kilo, indique cuantos potes"))
+        total += cantidad(seleccionarCantidad, 1200)
+      break;
+      case 2:
+        seleccionarCantidad = Number(prompt("el producto seleccionado es 1/2, indique cuantos potes"))
+        total += cantidad(seleccionarCantidad, 750)
+      break;
+    case 3:
+      seleccionarCantidad = Number(prompt("el producto seleccionado es 1/4, indique cuantos potes"))
+      total += cantidad(seleccionarCantidad, 450)
+     
+    break;
+    case 4:
+      seleccionarCantidad = Number(prompt("el producto seleccionado es cucurucho, cuantos potes"))
+      total += cantidad(seleccionarCantidad, 300)
 
-/*
-let ingresarNumero = parseInt (prompt("ingresar Numero")) ;
-
-for (let n = 2; n <= 10; N++) {
-
-    let resultado = ingresarNumero * n ;
-
-
-
-alert (ingresarNumero + "X" + n +"="+ resultado)
-
+    default:
+      break;
+  }
+  seleccionarProductos = Number(prompt( "1-Kilo $1200 2-Medio kilo $750 3-Cuarto kilo $450 4-Cucurucho $300 "))
 }
-*/
 
 
-
- let entrada = prompt("Ingresar un nombre").toUpperCase();
-
- while(entrada != "ESC" ){
-    switch (entrada) {
-       case "IGNACIO":
-            alert("HOLA IGNACIO");
-            break;
-
-        case "ISMAEL":
-            alert("HOLA ISMAEL");
-             break;
-        
-             case "AYELEN":
-            alert("HOLA AYELEN");
-             break;
-
-        default:
-            alert("¿QUIÉN SOS?")
-            break;
-    }
-    entrada = prompt("Ingresar un nombre");
- }
-
- alert("Gracias!!!")
+alert("el total de la compra es de: " + total)
 
 
